a093c00ce2975a2d7e7d2510d74c28bc49238189
Simon <175155691+Simon-Weij@users.noreply.github.com>
2025-10-31 17:09:29 +0100

    Nix

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+  outputs = { self, nixpkgs }: 
+  let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in {
+    packages.x86_64-linux.default = pkgs.writeShellApplication {
+      name = "cottonfetch";
+      
+      runtimeInputs = [
+        pkgs.pciutils
+        pkgs.mesa-demos
+      ];
+
+      text = builtins.readFile ./cottonfetch.sh;
+      checkPhase = "";
+    };
+
+    apps.x86_64-linux.default = {
+      type = "app";
+      program = "${self.packages.x86_64-linux.default}/bin/cottonfetch";
+    };
+  };
+}
